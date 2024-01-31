62e15d981bc910fc0decc245a601ba1f9fccdf3f
taleartisan <taleartisan@MacBook-Pro-de-taleartisan.local>
2024-01-31 16:17:06 +0100

    initial-commit

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import "./App.css";
+import Logo from "./components/Logo.jsx";
+import Form from "./components/Form.jsx";
+import PackingList from "./components/PackingList.jsx";
+import Stats from "./components/Stats.jsx";
+
+function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleClearList() {
+    const confirmed = confirm("Are you sure you want to delete all the items?");
+    if (confirmed) {
+      setItems([]);
+    }
+  }
+
+  return (
+    <>
+      <div className="app">
+        <Logo />
+        <Form onAddItems={handleAddItems} />
+        <PackingList
+          items={items}
+          onDeleteItem={handleDeleteItem}
+          onToggleItem={handleToggleItem}
+          onClearList={handleClearList}
+        />
+        <Stats items={items} />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Logo.jsx
@@ -0,0 +1,5 @@
+function Logo() {
+  return <h1>🏝️ Vacay List 🧳 </h1>;
+}
+
+export default Logo;
